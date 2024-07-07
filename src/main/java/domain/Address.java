a71cfa08e457bb0d49ba82eec3e452ab038cd0dd
daniels41 <dsilva260405@gmail.com>
2024-07-08 00:22:43 +0100

    update Agenda

--- src/main/java/domain/Address.java
+++ src/main/java/domain/Address.java
@@ -1,4 +1,41 @@
 package domain;
 
 public class Address {
+
+    private String street;
+
+    private String zipcode;
+
+    private String city;
+
+    public Address(String street, String zipcode, String city) {
+        this.street = street;
+        this.zipcode = zipcode;
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
 }
--- src/main/java/domain/Agenda.java
+++ src/main/java/domain/Agenda.java
@@ -1,4 +1,18 @@
 package domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Agenda {
+
+    private final List<EntryAgenda> entriesAgendaList;
+
+    public Agenda() {
+        entriesAgendaList = new ArrayList<>();
+    }
+
+    public List<EntryAgenda> getEntriesAgendaList() {
+        return entriesAgendaList;
+    }
+
 }
